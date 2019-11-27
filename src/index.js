84af8ec6b208d5b74d029b713ce0bb308f6e3404
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro.local>
2019-11-27 14:54:07 +0200

    map tile animation using snap(shit)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
-const $ = require(`jquery`);
-$(".region").attr('fill', "pink")
-    .attr('transform', "skewX(-20), scale(1, 0.8), translate(300, 10)");
+const Snap = require(`snapsvg`);
 
-$(".region > path").on('mouseenter', function() {
-        $(this).attr('transform', "translate(-10, -10)")
-            .parent().append($(this));
-    }).on('mouseleave', function() {
-        $(this).attr('transform', "translate(0, 0)");
-    }).attr('fill', "url(#diagonalHatch)");
+function move(el, transform) {
+  Snap(el).animate({ transform }, 1000, mina.easeinout);
+}
 
-const regions = Snap(".region > path");
-regions.drag(function() {console.log(this)}, function() {console.log(this)}, function() {console.log(this)});
-console.log(1);
+const map = Snap.select("svg");
+
+const regions = map.selectAll(".region");
+
+regions.forEach(region => {
+  region.hover(
+    e => move(e.currentTarget, `t0,-45r0`),
+    e => move(e.currentTarget, `t0,0r0`)
+  );
+});
